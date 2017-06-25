e67e0a05d11666e4a4d9420db5f0dee177863f04
Katarzyna Wojcik <wojcik.katarzyna.danuta@gmail.com>
2017-06-25 12:37:48 +0200

    add boxes in alphabet

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
 
+    // MOBILE MENU:
     let hamburger = $('.mobileMenu');
-    console.log(hamburger);
 
     hamburger.on('click', function(e){
       e.preventDefault();
       $(this).next().toggleClass('showMenu');
     })
+
+
+    //BOX'S BACKGROUND IN ALPHABET SECTION:
+    let boxes = $('#alphabet .box');
+    console.log(boxes);
+
+    for (let i=0; i<boxes.length; i++){
+        let letter = $(this).children().first().text();
+        $(this).attr('background-image','url("../images/B.jpg")');
+    }
+
 
 
 })
