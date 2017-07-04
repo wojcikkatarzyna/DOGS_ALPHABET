03cc536efd0e0fa93ba27ad0ca8765ead0d76d1a
Katarzyna Wojcik <wojcik.katarzyna.danuta@gmail.com>
2017-07-04 15:08:12 +0200

    add event on map

--- js/app.js
+++ js/app.js
@@ -74,6 +74,89 @@ $(document).ready(function(){
         // let newDiv = $('<div class = "address">');
       })
 
+      //CHOSE PROVINCE FROM MAP:
+
+      const mainMap = $('svg');
+      console.log(mainMap);
+
+      mainMap.on('click', function(e){
+        e.preventDefault();
+        let ind = 16;
+        let path = e.target.id;
+        switch( e.target.id) {
+          case 'POL3141' :
+              ind = 0;
+              break;
+
+          case 'POL3145' :
+              ind = 1;
+              break;
+
+          case 'POL3151' :
+              ind = 2;
+              break;
+
+          case 'POL3143' :
+              ind = 3;
+              break;
+
+          case 'POL3147' :
+              ind = 4;
+              break;
+
+          case 'POL3170' :
+              ind = 5;
+              break;
+
+          case 'POL3148' :
+              ind = 6;
+              break;
+
+          case 'POL3167' :
+              ind = 7;
+              break;
+
+          case 'POL3152' :
+              ind = 8;
+              break;
+
+          case 'POL3150' :
+              ind = 9;
+              break;
+
+          case 'POL3140' :
+              ind = 10;
+              break;
+
+          case 'POL3146' :
+              ind = 11;
+              break;
+
+          case 'POL3149' :
+              ind = 12;
+              break;
+
+          case 'POL3139' :
+              ind = 13;
+              break;
+
+          case 'POL3144' :
+              ind = 14;
+              break;
+
+          case 'POL3142' :
+              ind = 15;
+              break;
+         }
+         if (ind < 16) {
+            let urlMap = sheltersMaps[ind].src;
+            console.log(urlMap);
+            shelterList.empty();
+            let newImg = $('<img src="'+urlMap+'">');
+            shelterList.append(newImg);
+         }
+
+      })
 
 
 })
